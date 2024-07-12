74fc5909aa0ab31cf4f5eb4e8301b1626abe88a5
Vinh Bui <vinh.buiquangvinh@hcmut.edu.vn>
2024-07-12 10:33:36 +0700

    First Commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+function AddDevice(){
+    document.getElementById("AddBox").style.visibility ="visible";
+    document.getElementById("OutAddBox").style.visibility ="visible";
+};
+function RemoveDevice(){
+    document.getElementById("AddBox").style.visibility ="hidden";
+    document.getElementById("OutAddBox").style.visibility ="hidden";
+};
